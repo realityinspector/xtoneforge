65a01beee8d4a1ad3b5c36725b3def4eb20cc7fb
Adam King <adamjking3@gmail.com>
2026-03-06 11:14:47 -1000

    Remove .astro build cache from git, add Medium blog post
    The .astro/ directory is auto-generated and already gitignored but was
    previously committed. Also adds Medium cross-post version of the blog.

--- apps/website/.astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/blog/stoneforge-deep-dive.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstoneforge-deep-dive.mdx&astroContentModuleFlag=true")],
-["src/content/blog/introducing-stoneforge.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fintroducing-stoneforge.mdx&astroContentModuleFlag=true")]]);
-		
